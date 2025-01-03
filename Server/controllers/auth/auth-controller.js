7a6d6312f0837bfcdfca4359291eac0bf1b232fa
Alex5748 <abpalex05@gmail.com>
2025-01-04 01:16:10 +0545

    third

---
+++ Server/controllers/auth/auth-controller.js
@@ -0,0 +1,90 @@
+const bcrypt = require('bcryptjs');
+const jwt = require('jsonwebtoken');
+const User = require('../../models/User');
+
+
+///register
+const registerUser = async (req, res) => {
+    const { userName, email, password } = req.body;
+    
+    try {
+        const hashPassword = await bcrypt.hash(password, 12)
+        const newUser = new User({
+            userName,
+            email,
+            password: hashPassword
+        });
+
+        await newUser.save();
+        res.send(200).json({
+            success: true,
+            message: 'Registration Successful'
+        });
+        
+    } catch (e) {
+        console.log(e);
+        res.status(500).json({
+            success: false,
+            message:"some Error Occured"
+        })
+        
+    }
+}
+
+
+
+
+//login
+
+const login = async(req, res) => {
+
+
+
+    try {
+        
+    } catch (e) {
+        console.log(e);
+        res.status(500).json({
+            success: false,
+            message:"some Error Occured"
+        })
+        
+    }
+    
+}
+
+
+
+
+
+
+
+
+
+
+//logout
+
+
+
+
+
+
+//auth-middleware
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+module.exports = { registerUser };
